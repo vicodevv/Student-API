bec2e3b2fed7b36c2d0c2a790ae0806671fec8dd
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-27 17:32:08 +0100

    added student to db

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -15,23 +18,41 @@ public class DemoApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository){
+	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate){
 		return args -> {
 			Address address = new Address(
 				"Nigeria",
 				"Lagos",
 				"SW1A 1AA"
 			);
+			String email = "vomoniyi2001@gmail.com";
 			Student student = new Student(
 				"Victor", 
 				"Omoniyi",
-				"vomoniyi2001@gmail.com", 
+				email, 
 				Gender.MALE,
 				address,
 				"Computer Science", 
 				LocalDateTime.now()
 			);
-		repository.insert(student);
+
+		//	Query query = new Query();
+		//	query.addCriteria(Criteria.where("email").is(email));
+			
+		//	List<Student> students = MongoTemplate.find(query, Student.class);
+		//	if (students.size() > 1){
+		//		throw new IllegalStateException("Student already exists" + email);
+		//	}
+		//	if (students.isEmpty()){
+		//		System.out.println("Inserting Student" + student);
+		//		repository.insert(student);
+		//	}
+		//	else{ 
+		//		System.out.println(student + "already exists");  
+		//	}
+		//	repository.insert(student);
+		//		repository.save(student);
 		};
+
 	}
 }
